8a4dc0b89806e0c19ad7d5d8e0f0dfd7ccf48d33
John Wilson <john66@talk21.com>
2019-03-14 16:03:52 +0000

    add ability to give names on URL using player1 and player2 parameters

--- js/dice.js
+++ js/dice.js
@@ -1,12 +1,29 @@
-
 function throwDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-var player1DiceThrow = throwDice() ;
-var player2DiceThrow = throwDice() ;
-var gameResult = (player1DiceThrow == player2DiceThrow) ? "Draw" :
-  (player1DiceThrow > player2DiceThrow ? "Player 1 Wins" : "Player 2 Wins!");
+
+var player1Name = "Player 1";
+var player2Name = "Player 2";
+
+try {
+
+  var [par1Str, par2Str] = window.location.href.split("?")[1].split('&');
+
+  var pars1 = par1Str.split("=");
+  var pars2 = par2Str.split("=");
+
+  player1Name = (pars1[0] == 'player1' ? pars1[1] : pars2[1]);
+  player2Name = (pars2[0] == 'player2' ? pars2[1] : pars1[1]);
+
+} catch (e) {
+
+}
+
+var player1DiceThrow = throwDice();
+var player2DiceThrow = throwDice();
+var gameResult = (player1DiceThrow == player2DiceThrow) ? "Draw!" :
+  (player1DiceThrow > player2DiceThrow ? player1Name + " Wins!" : player2Name + " Wins!");
 
 var player1DiceeImage = `images/dice${player1DiceThrow}.png`;
 var player2DiceeImage = `images/dice${player2DiceThrow}.png`;
@@ -20,9 +37,14 @@ try {
     images[0].setAttribute("src", player1DiceeImage);
     images[1].setAttribute("src", player2DiceeImage);
   } else {
-      throw Error("Bad HTML Format Page - ") ;
+    throw Error("Bad HTML Format Page - ");
   }
   document.querySelector(".gameResult").innerText = gameResult;
+
+  document.querySelector("#player1Name").innerText = player1Name ;
+  document.querySelector("#player2Name").innerText = player2Name ;
+
+
 
 } catch (e) {
   alert('Exception --->' + e.message);
