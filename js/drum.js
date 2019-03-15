5e93b43cb053ff6a757175401253dd2f49ba3911
John Wilson <john66@talk21.com>
2019-03-15 14:28:53 +0000

    Add keyboard events to drum kit playing

--- js/drum.js
+++ js/drum.js
@@ -1,3 +1,36 @@
+function playAudioAsset(key) {
+  var audioAsset;
+
+  switch (key.toLowerCase()) {
+    case 'w':
+      audioAsset = "sounds/tom-1.mp3";
+      break;
+    case 'a':
+      audioAsset = "sounds/tom-2.mp3";
+      break;
+    case 's':
+      audioAsset = "sounds/tom-3.mp3";
+      break;
+    case 'd':
+      audioAsset = "sounds/tom-4.mp3";
+      break;
+    case 'j':
+      audioAsset = "sounds/snare.mp3";
+      break;
+    case 'k':
+      audioAsset = "sounds/kick-bass.mp3";
+      break;
+    case 'l':
+      audioAsset = "sounds/crash.mp3";
+      break;
+    default:
+      break;
+  }
+  if (typeof(audioAsset) != 'undefined') {
+    var audio = new Audio(audioAsset);
+    audio.play();
+  }
+}
 
 function initCallBacks() {
 
@@ -13,27 +46,36 @@ function initCallBacks() {
         // we are only allowing to play one sound at a time!
 
         if (clst.contains('w')) {
-          audioAsset = "sounds/tom-1.mp3";
-        } else if (clst.contains('a')) {
-          audioAsset = "sounds/tom-2.mp3";
-        } else if (clst.contains('s')) {
-          audioAsset = "sounds/tom-3.mp3";
-        } else if (clst.contains('d')) {
-          audioAsset = "sounds/tom-4.mp3";
-        } else if (clst.contains('j')) {
-          audioAsset = "sounds/snare.mp3";
-        } else if (clst.contains('k')) {
-          audioAsset = "sounds/kick-bass.mp3";
-        } else if (clst.contains('l')) {
-          audioAsset = "sounds/crash.mp3";
-        }
+          playAudioAsset('w') ;
 
-        var audio = new Audio(audioAsset);
-        audio.play();
+        } else if (clst.contains('a')) {
+          playAudioAsset('a') ;
+
+        } else if (clst.contains('s')) {
+          playAudioAsset('s') ;
+
+        } else if (clst.contains('d')) {
+          playAudioAsset('d') ;
+
+        } else if (clst.contains('j')) {
+          playAudioAsset('j') ;
+
+        } else if (clst.contains('k')) {
+          playAudioAsset('k') ;
+
+        } else if (clst.contains('l')) {
+          playAudioAsset('l') ;
+
+        }
 
 
       }
     );
+    this.addEventListener("keydown", function(evt) {
+      //console.log(evt);
+      playAudioAsset(evt.key) ;
+    })
+
   }
 
 }
