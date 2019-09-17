91f0e8177a421950e4e804b253939db874c6f543
John Wilson <john66@talk21.com>
2019-09-17 17:52:24 +0100

    added support

---
+++ js/boards.js
@@ -0,0 +1,10 @@
+
+var boardsData = `[ 
+	{"image":"boards1.jpg","heading":"heading1 text", "emphasis" :"Some Emphasis1"}, 
+	{"image":"boards1.jpg","heading":"heading2 text", "emphasis" :"Some Emphasis2"}, 
+	{"image":"boards1.jpg","heading":"heading3 text", "emphasis" :"Some Emphasis3"}, 
+	{"image":"boards1.jpg","heading":"heading4 text", "emphasis" :"Some Emphasis4"}, 
+	{"image":"boards1.jpg","heading":"heading5 text", "emphasis" :"Some Emphasis5"}
+
+]`;
+
