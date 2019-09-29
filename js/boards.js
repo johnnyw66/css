3eb5871d6df7f34de227df065765b0e7a6203e91
John Wilson <john66@talk21.com>
2019-09-29 10:11:21 +0100

    new walk added, improvements to adding in sections. Now done dynamically

--- js/boards.js
+++ js/boards.js
@@ -1,38 +1,38 @@
 
-var boardsData = `[ 
-	
-	{ "image" : "8-bit-adder.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+var boardsData = `[
+
+	{ "image" : "8-bit-adder.jpg", "heading" : "Number 1 8-bit-adder heading text", "emphasis" : "some emphasis"},
 	{ "image" : "8-bit-bus-selector.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "8-bit-bus-selector2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	
+
+
 	{ "image" : "4bit-demux.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "4bit-counter.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "ram-bank.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "4bit-counter.jpg", "heading" : "4 bit counter text", "emphasis" : "some emphasis on 4 bit counter"},
+	{ "image" : "ram-bank.jpg", "heading" : "ram-bank text", "emphasis" : "some emphasis ram-bank"},
 	{ "image" : "selectable-4-reg.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "register-bank.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "instruction-reg1.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "register-bank.jpg", "heading" : "register-bank heading text", "emphasis" : "some emphasis register-bank"},
+	{ "image" : "instruction-reg1.jpg", "heading" : "instruction-reg1 heading text", "emphasis" : "some emphasis instruction-reg1"},
 	{ "image" : "instruction-reg2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	
+
+
 	{ "image" : "combi-logic1.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "combi-logic2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "combi-logic3.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "combinational-logic.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "combinational-logic.jpg", "heading" : " combinational-logic heading text", "emphasis" : "combinational-logic some emphasis"},
 	{ "image" : "combi-logic-tester.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	{ "image" : "instruction-reg3.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "literals.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "8-bit-adder2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "ones-complement.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "parity-zero-test.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 
-	{ "image" : "ram-programmer2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "ram-programmer1.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "instruction-reg3.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "literals.jpg", "heading" : "literals text", "emphasis" : "literals emphasis"},
+	{ "image" : "8-bit-adder2.jpg", "heading" : "8-bit-adder heading text", "emphasis" : "some emphasis 8-bit-adder"},
+	{ "image" : "ones-complement.jpg", "heading" : "ones-complement text", "emphasis" : "some emphasis"},
+	{ "image" : "parity-zero-test.jpg", "heading" : "parity-zero-test text", "emphasis" : "some emphasis"},
+
+	{ "image" : "ram-programmer2.jpg", "heading" : "rom-program text", "emphasis" : "some emphasis"},
+	{ "image" : "ram-programmer1.jpg", "heading" : "rom-program text", "emphasis" : "some emphasis"},
 	{ "image" : "rom-program.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	{ "image" : "ring-counter.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
+
+	{ "image" : "ring-counter.jpg", "heading" : "ring-counter text", "emphasis" : "some emphasis"},
+
 	{ "image" : "8-bit-hex-display-micro-view.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "8-bit-input-micro-view.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "hex-display2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
@@ -42,25 +42,24 @@ var boardsData = `[
 	{ "image" : "hex-display.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "decimal-display1.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "8-bit-reg-with-display.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "8-bit-reg-with-bar-display.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	{ "image" : "8-bit-reg3.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "8-bit-reg-with-bar-display.jpg", "8-bit-reg-with-bar-display" : "8-bit-reg-with-bar-display text", "emphasis" : "some emphasis"},
+
+	{ "image" : "8-bit-reg3.jpg", "heading" : "8-bit-reg3 text", "emphasis" : "some emphasis"},
 	{ "image" : "8-bit-reg2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
 	{ "image" : "clock-programmable.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "clock.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	{ "image" : "hex-input.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "hex-input3.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "hex-input2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "hex-input6.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "hex-input5.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "hex-input4.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	
-	{ "image" : "common-bus.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "bus.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
+	{ "image" : "clock.jpg", "heading" : "clock text", "emphasis" : "some emphasis"},
+
+	{ "image" : "hex-input.jpg", "heading" : "hex-input text", "emphasis" : "some emphasis"},
+	{ "image" : "hex-input3.jpg", "heading" : "hex-input3 text", "emphasis" : "some emphasis"},
+	{ "image" : "hex-input2.jpg", "heading" : "hex-input2 text", "emphasis" : "some emphasis"},
+	{ "image" : "hex-input6.jpg", "heading" : "hex-input6 text", "emphasis" : "some emphasis"},
+	{ "image" : "hex-input5.jpg", "heading" : "hex-input5 text", "emphasis" : "some emphasis"},
+	{ "image" : "hex-input4.jpg", "heading" : "hex-input4 text", "emphasis" : "some emphasis"},
+
+	{ "image" : "common-bus.jpg", "heading" : "common-bus text", "emphasis" : "some emphasis"},
+	{ "image" : "bus.jpg", "heading" : "bus text", "emphasis" : "some emphasis"},
 	{ "image" : "memory-address-reg1.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "4bit-demux2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"},
-	{ "image" : "rom-program2.jpg", "heading" : "heading text", "emphasis" : "some emphasis"}
+	{ "image" : "4bit-demux2.jpg", "heading" : "4bit-demux2 text", "emphasis" : "some emphasis"},
+	{ "image" : "rom-program2.jpg", "heading" : "rom-program2 text", "emphasis" : "some emphasis"}
 
 ]`;
-
